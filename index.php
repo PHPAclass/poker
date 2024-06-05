3549bef20bc04a8966564a86689ab322135452fc
JHSAND <shotaro0106t@gmail.com>
2024-06-05 14:20:40 +0900

    index page

--- index.php
+++ index.php
@@ -4,7 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Poker</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
+    <header>
+        <h1>Poker</h1>
+    </header>
+    <section class="home">
+        <img src="./img/poker_logo.png" alt="poker logo" class="poker_logo">
+        <form action="" method="POST">
+            <button type="submit" name="play">ゲームスタート</button>
+        </form>
+    </section>
 </body>
 </html>
