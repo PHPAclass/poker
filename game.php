8746c4be34b24478bdc4cf78c3df32f6a340027d
GigaOwO <takumagiga1118@gmail.com>
2024-06-06 15:08:01 +0900

    game

--- game.php
+++ game.php
@@ -12,38 +12,40 @@ main();
     <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-    <section>
+    <section class="player3">
         <p>プレイヤー3</p>
         <div>
             <img src="./img/cards/<?= $_SESSION['used'][0]?>" alt="トランプ">
             <img src="./img/cards/<?= $_SESSION['used'][1] ?>" alt="トランプ">
         </div>
     </section>
-    <section>
-        <p>プレイヤー2</p>
-        <div>
-            <img src="./img/cards/<?= $_SESSION['used'][2] ?>" alt="トランプ">
-            <img src="./img/cards/<?= $_SESSION['used'][3] ?>" alt="トランプ">
-        </div>
-    </section>
-    <section>
-        <p>プレイヤー4</p>
-        <div>
-            <img src="./img/cards/<?= $_SESSION['used'][4] ?>" alt="トランプ">
-            <img src="./img/cards/<?= $_SESSION['used'][5] ?>" alt="トランプ">
-            <img src="./img/cards/<?= $_SESSION['used'][6] ?>" alt="トランプ">
-            <img src="./img/cards/<?= $_SESSION['used'][7] ?>" alt="トランプ">
-            <img src="./img/cards/<?= $_SESSION['used'][8] ?>" alt="トランプ">       
-        </div>
-        <p>ポット:<?= $pot_point ?></p>
-    </section>
-    <section>
-        <div>
-            <img src="./img/cards/<?= $_SESSION['used'][9] ?>" alt="トランプ">
-            <img src="./img/cards/<?= $_SESSION['used'][10] ?>" alt="トランプ">
-        </div>
-    </section>
-    <section>
+    <div class="middle">
+        <section class="player2">
+            <p>プレイヤー2</p>
+            <div>
+                <img src="./img/cards/<?= $_SESSION['used'][2] ?>" alt="トランプ">
+                <img src="./img/cards/<?= $_SESSION['used'][3] ?>" alt="トランプ">
+            </div>
+        </section>
+        <section class="pot">
+            <div>
+                <img src="./img/cards/<?= $_SESSION['used'][4] ?>" alt="トランプ">
+                <img src="./img/cards/<?= $_SESSION['used'][5] ?>" alt="トランプ">
+                <img src="./img/cards/<?= $_SESSION['used'][6] ?>" alt="トランプ">
+                <img src="./img/cards/<?= $_SESSION['used'][7] ?>" alt="トランプ">
+                <img src="./img/cards/<?= $_SESSION['used'][8] ?>" alt="トランプ">       
+            </div>
+            <p>ポット:<?= $pot_point ?></p>
+        </section>
+        <section class="player4">
+            <p>プレイヤー4</p>
+            <div>
+                <img src="./img/cards/<?= $_SESSION['used'][9] ?>" alt="トランプ">
+                <img src="./img/cards/<?= $_SESSION['used'][10] ?>" alt="トランプ">
+            </div>
+        </section>
+    </div>
+    <section class="player1">
         <div>
             <img src="./img/cards/<?= $_SESSION['used'][11] ?>" alt="トランプ">
             <img src="./img/cards/<?= $_SESSION['used'][12] ?>" alt="トランプ">
